ed82de69f20e300dcfe67fb4f7b06c998922d0d1
sternd <sterndonald@gmail.com>
2020-02-25 13:34:20 -0800

    Adding setup and moving files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PythonModules',
+      version='1.0.0',
+      description='Python modules for use in other projects',
+      author='dstern',
+      packages=['modules']
+     )
